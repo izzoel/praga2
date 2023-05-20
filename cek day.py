01181ac9f32bf12265cd8d94ce64459259a5507e
izzoel <ramadhani.zulfahmi@gmail.com>
2023-05-20 23:43:34 +0800

    add gaji in excal

---
+++ cek day.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+# If today is Friday (0 = Mon, 1 = Tue, 2 = Wen ...)
+# if datetime.today().weekday() == 4:
+if  datetime(2023, 5, 19).weekday() == 4:
+    print("Yes, Today is Friday")
+else:
+    print("Nope...")
